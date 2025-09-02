caab38c4ffde5b437ace7df22b8cadfe2d3733b1
juniorlacerda <juniorlacerdasilva@gmail.com>
2025-09-02 07:42:29 -0300

    criada a rota POST /blocks em blockchainServers

--- protochain/__tests__/block.test.ts
+++ protochain/__tests__/block.test.ts
@@ -7,23 +7,43 @@ describe("Block tests", () => {
   let genesis: Block;
 
   beforeAll (() => {
-    genesis = new Block(0, "", "Genesis Block");
+    genesis = new Block({
+      data: "Genesis Block"
+    } as Block);
   })
   
   test('Should be valid', () => {
-    const block = new Block(1, genesis.hash, 'block 2');
+    const block = new Block({
+          index: 1, 
+          previousHash: genesis.hash, 
+          data: "Block 2"
+        } as Block);
     const valid = block.isValid(genesis.hash, genesis.index);
     expect(valid.success).toBe(true);
   });
 
+  test('Should NOT be valid', () => {
+    const block = new Block();
+    const valid = block.isValid(genesis.hash, genesis.index);
+    expect(valid.success).toBeFalsy();
+  });
+
   test('Should be NOT valid (previous hash)', () => {
-    const block = new Block(1, "abc", 'block 2');
+    const block = new Block({
+          index: 1, 
+          previousHash: "abc", 
+          data: "Block 2"
+        } as Block);
     const valid = block.isValid(genesis.hash, genesis.index);
     expect(valid.success).toBe(false);
   });
 
   test('Should be NOT valid (timestamp)', () => {
-    const block = new Block(1, genesis.hash, 'block 2');
+    const block = new Block({
+          index: 1, 
+          previousHash: genesis.hash, 
+          data: "Block 2"
+        } as Block);
     block.timestamp = -1;
     block.hash = block.getHash();
     const valid = block.isValid(genesis.hash, genesis.index);
@@ -31,20 +51,32 @@ describe("Block tests", () => {
   });
 
   test('Should be NOT valid (hash)', () => {
-    const block = new Block(1, genesis.hash, 'block 2');
+    const block = new Block({
+          index: 1, 
+          previousHash: genesis.hash, 
+          data: "Block 2"
+        } as Block);
     block.hash = "";
     const valid = block.isValid(genesis.hash, genesis.index);
     expect(valid.success).toBe(false);
   });
 
   test('Should be NOT valid (data)', () => {
-    const block = new Block(1, genesis.hash, '');
+    const block = new Block({
+          index: 1, 
+          previousHash: genesis.hash, 
+          data: ""
+        } as Block);
     const valid = block.isValid(genesis.hash, genesis.index);
     expect(valid.success).toBe(false);
   });
 
   test('Should be NOT valid (index)', () => {
-    const block = new Block(-1, genesis.hash, 'block 2');
+    const block = new Block({
+          index: -1, 
+          previousHash: genesis.hash, 
+          data: "Block 2"
+        } as Block);
     const valid = block.isValid(genesis.hash, genesis.index);
     expect(valid.success).toBe(false);
   });
